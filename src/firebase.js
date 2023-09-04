e281ba98e41fc168612fb1250dec6c74f8842bc1
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2023-09-04 23:55:28 +0900

    #1 Setting: React_Twitter 개발 환경 세팅

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjSi6ro1DQNBrP91oH-5nAn2oeSnWjYWQ",
+  authDomain: "react-twitter-3bd30.firebaseapp.com",
+  projectId: "react-twitter-3bd30",
+  storageBucket: "react-twitter-3bd30.appspot.com",
+  messagingSenderId: "735004424034",
+  appId: "1:735004424034:web:85f764e67456247487de3b",
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);

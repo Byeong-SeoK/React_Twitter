af23d8f784cf3561b82fce8f6779078e85a4d842
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2023-09-05 18:56:07 +0900

    #7 Setting, Refactor: jsconfig.json 파일 생성 & Firebase Auth 연결

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "myBase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  //authService.currentUser를 통해서 현재 접근하는 User가
+  //권한이 있는 User면 User객체를 반환하고 권한이 없으면 null을 반환한다.
+  //그렇기 때문에 isLoggedIn의 초기값으로 줄 수 있다.
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; React_Twitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;

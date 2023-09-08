81c35ccd64baf37db0e474e8e820ddd90ffd2c84
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2023-09-09 00:46:42 +0900

    Feat: Social Login API연동 (Google & Github)

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,12 @@
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
+import { authService } from "myBase";
 import React, { useState } from "react";
 
 const Auth = () => {
@@ -50,6 +54,28 @@ const Auth = () => {
   const toggleAccount = () => setNewAccount((prev) => !prev);
   //이 toggleAccount는 log in과 create account를 바꿀 수 있게 해주는 것이다.
 
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    //button의 onClick에서 발생한 event 객체안의 target key에 대한 value값을
+    //받아서 객체 안에 저장하도록 한다.
+
+    let provider;
+    //provider는 firebase의 social login API를 받는 변수이다.
+    //각 social login 함수마다 가지고 있는 값이 다른다.
+
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+      console.log(provider);
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -76,8 +102,12 @@ const Auth = () => {
         {newAccount ? "Sign in." : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
